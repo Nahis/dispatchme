213d90382da86bdb098b6c49286741fbf3e624c7
Nahis <nahi@dispatch.me>
2020-02-25 23:57:23 +0200

    Update CustomPayload.js

--- dispatch-connect/samples/DispatchInbound/JavaScript/CustomPayload.js
+++ dispatch-connect/samples/DispatchInbound/JavaScript/CustomPayload.js
@@ -2,20 +2,16 @@ function post() {
     const zlib = require('zlib');
     var crypto = require('crypto');
 
-    var secret = 'your_secret';
     var key = 'your_key';
+    var secret = 'your_secret';
 
-    var payload = zlib.gzipSync('Hello, universe!');
+    var payload = zlib.gzipSync('Hello, world!');
     var secret = Buffer.from(secret, 'hex');
     var signature = crypto.createHmac('sha256', secret).update(payload, 'utf8').digest('hex');
-    var request = {
-      url: 'https://connect-sbx.dispatch.me/agent/in',
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'X-Dispatch-Key': key,
-        'X-Dispatch-Signature': signature
-      },
-      data: payload
-    }; 
+
+    xhttp.open("POST", "https://connect-sbx.dispatch.me/agent/in", true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+    xhttp.setRequestHeader("X-Dispatch-Key", key);
+    xhttp.setRequestHeader("X-Dispatch-Signature", signature);
+    xhttp.send(payload);    
 }
