da2831feb9b8e7050d0620c87d271daa39a05d4d
Nahis <nahi@dispatch.me>
2021-01-19 22:32:08 +0200

    Update download_attachment.py

--- dispatch-connect/samples/DispatchOutbound/Python/download_attachment.py
+++ dispatch-connect/samples/DispatchOutbound/Python/download_attachment.py
@@ -1,7 +1,7 @@
 import shutil
 import requests
 
-token_id = 'token_id'
+token_id = "token_id"
 file = requests.get("http://files-api.dispatch.me/v1/datafiles/%s" % token_id, stream=True)
 extension = file.headers["content-type"].split("/")[1]
 with open("%s/images/%s.%s" % ("working_folder", token_id, extension), "wb") as f:
