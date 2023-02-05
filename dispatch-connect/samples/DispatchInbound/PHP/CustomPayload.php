53a57f5cdc87468b93d465da92ced79f5304c3bf
Nahis <nahi@dispatch.me>
2023-02-05 23:04:05 +0200

    update php example

--- dispatch-connect/samples/DispatchInbound/PHP/CustomPayload.php
+++ dispatch-connect/samples/DispatchInbound/PHP/CustomPayload.php
@@ -8,28 +8,21 @@ $compressed = gzencode(utf8_encode(json_encode($payloadData)));
 $secret = hex2bin($secret);
 $sign = hash_hmac('sha256', $compressed, $secret, true);
 $sign = utf8_decode(bin2hex($sign));
-$curl = curl_init();
-curl_setopt_array($curl, array(
-    CURLOPT_URL => "https://connect-sbx.dispatch.me/agent/in",
-    CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_ENCODING => "",
-    CURLOPT_MAXREDIRS => 10,
-    CURLOPT_TIMEOUT => 0,
-    CURLOPT_FOLLOWLOCATION => true,
-    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-    CURLOPT_CUSTOMREQUEST => "POST",
-    CURLOPT_POSTFIELDS => $compressed,
-    CURLOPT_HTTPHEADER => array(
-        "Content-Type: application/json",
-        "X-Dispatch-Key: $key",
-        "X-Dispatch-Signature: $sign",
-        "MessageId": "unique_id_for_transaction"  // optional                
-        "RecordType: job"  // The would be `organization`, `user` etc. (any identifying value is acceptable) depending on what you're trying to send over. Refer to the playbook
-    ),
-));
-$response = curl_exec($curl);
-curl_close($curl);
+$opts = [
+    "http" => [
+        "method" => "POST",
+        "header" => "Content-Type: application/json\r\n" .
+            "X-Dispatch-Key: $key\r\n" .
+            "X-Dispatch-Signature: $sign\r\n" .
+            "MessageId: unique_id_for_transaction\r\n" .  // optional but must be unique value for each transaction if passed
+            "RecordType: job\r\n", // The would be `organization`, `user` etc. (any identifying value is acceptable) depending on what you're trying to send over. Refer to the playbook
+        'content' => $compressed
+    ]
+];
 
-echo $response;
+$context = stream_context_create($opts);
+$result = file_get_contents('https://connect-sbx.dispatch.me/agent/in', false, $context);
+echo $result
+
 
 ?>
